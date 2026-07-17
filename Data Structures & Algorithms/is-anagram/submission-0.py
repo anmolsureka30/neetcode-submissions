02f22f7390ece9930f34a965c92e3de48516fd8c
Anmol Sureka <24b2470@iitb.ac.in>
2026-07-17 15:41:33 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        slist=list(s)
+        tlist=list(t)
+        a=[]
+        b=[]
+        for x in slist:
+            a.append(ord(x)-ord('a'))
+        a.sort()   
+        for y in tlist:
+            b.append(ord(y)-ord('a'))
+        b.sort() 
+        if a==b:
+            return True
+        else:
+            return False       
+
+
